4c96b138f217b117c26339a48e55513c42698ce2
Twosdayz <115088672+Twosdayz@users.noreply.github.com>
2022-10-05 08:36:27 -0500

    Update content_script.js
    Updated text

--- Source/content_script.js
+++ Source/content_script.js
@@ -39,10 +39,10 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bElon Musk\b/g, "Butt Face");
+	v = v.replace(/\bElon musk\b/g, "Butt Face");
+	v = v.replace(/\belon Musk\b/g, "Butt Face");
+	v = v.replace(/\belon musk\b/g, "Butt Face");
 	
 	textNode.nodeValue = v;
 }
